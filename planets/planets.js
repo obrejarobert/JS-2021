4e9026489ef367f420a95b385ad7d42e10634bf4
Robert <obrejadenisYT@gmail.com>
2021-02-12 18:46:15 +0200

    planet

--- planets/planets.js
+++ planets/planets.js
@@ -1,10 +1,26 @@
+let button = document.getElementById("check")
 let planet = document.getElementById("redplanet")
 
-planet.classList.add("alert")
+button.addEventListener("click", function () {
+    planet.classList.add("alert")
 
-planet.innerText = "Alien invasion"
+    planet.innerText = "Alien invasion"
 
-// planet.style.fontWeight = "bold"
-// planet.style.color = "red"
+    // planet.style.fontWeight = "bold"
+    // planet.style.color = "red"
 
-// planet.remove()
+    // planet.remove()
+})
+
+document.getElementById("reset")
+    .addEventListener("click", function () {
+        planet.classList.add("alert")
+
+        planet.innerText = "Nothing to report"
+        planet.classList.remove("alert")
+
+        // planet.style.fontWeight = "bold"
+        // planet.style.color = "red"
+
+        // planet.remove()
+    })
