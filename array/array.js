8b66960639cf0cd5403dbf673c87e92bfd8da93d
Robert <obrejadenisYT@gmail.com>
2021-02-18 11:44:46 +0200

    array

---
+++ array/array.js
@@ -0,0 +1,12 @@
+let bubbles = [60, 59, 48, 71, 45, 51, 65, 70, 49, 38]
+
+let fruits = ["ananas", "mango", "orange", "tomato", "cucumber"]
+
+bubbles.sort()
+max = bubbles.pop()
+
+fruits.sort()
+fruits.reverse()
+
+console.log(fruits)
+console.log(max)
