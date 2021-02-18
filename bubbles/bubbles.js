e9a842b1f40392ae3eee1d56c2aa09fb8ba43487
Robert <obrejadenisYT@gmail.com>
2021-02-18 14:08:33 +0200

    animatii

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -1,4 +1,4 @@
-var numberOfBubbles = 100
+var numberOfBubbles = 50
 for (let i = 0; i < numberOfBubbles; i++) {
     newBubble()
 }
@@ -7,9 +7,16 @@ function newBubble() {
     bubble.classList.add("bubble")
     let x = randomNumber(100)
     let delay = randomNumber(3000)
+    let red = randomNumber(255)
+    let green = randomNumber(255)
+    let blue = randomNumber(255)
+    let size = randomNumber(2) + 1
     bubble.style.left = x + "vw"
+    // bubble.style.left = `${x}wv`
     bubble.style.animationDelay = delay + "ms"
     document.querySelector("body").appendChild(bubble)
+    bubble.style.borderColor = `rgb(${red},${green},${blue})`
+    bubble.style.height = bubble.style.width = `${size}em`
 }
 function randomNumber(max) {
     return Math.floor(Math.random() * max);
